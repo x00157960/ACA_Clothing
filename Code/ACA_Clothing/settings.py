f2b34e79ce3fad108ee7b19b37cb5e1eb78cc413
X00157960 Callum Dennis <x00157960@COMPUTING.STU.IT-TALLAGHT.IE>
2020-02-18 12:37:07 +0000

    Revert "Added Users: Admin, Manager, Customer #AB26
    This reverts commit 217daf9a56618ed56ba30b171fe56c37bb6fcda9.

--- Code/ACA_Clothing/settings.py
+++ Code/ACA_Clothing/settings.py
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-uk'
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'GMT'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
