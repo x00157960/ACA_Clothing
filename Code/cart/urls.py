246565cf132ae0cc91fdd2d25b9ce84beaec620a
X00147311 Andrew Finegan <x00147311@COMPUTING.STU.IT-TALLAGHT.IE>
2020-02-26 15:27:33 +0000

    cart page AB#24

--- Code/cart/urls.py
+++ Code/cart/urls.py
@@ -5,4 +5,8 @@ from . import views
 urlpatterns = [
     path('charge/', views.charge, name='charge'),
     path('cart/', views.CartPageView.as_view(), name='cart'),
+    path('add/<int:product_id>/', views.add_cart, name='add_cart'),
+	path('', views.cart_detail, name='cart_detail'),
+    path('remove/<int:product_id>/', views.cart_remove, name='cart_remove'),
+    path('full_remove/', views.full_remove, name='full_remove'),
 ]
--- Code/cart/views.py
+++ Code/cart/views.py
@@ -1,7 +1,11 @@
 import stripe
 from django.conf import settings
 from django.views.generic.base import TemplateView
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from shop.models import Product
+from .models import Cart, CartItem
+from django.contrib import messages
+from django.core.exceptions import ObjectDoesNotExist
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
@@ -22,3 +26,67 @@ def charge(request):
             source=request.POST['stripeToken']
         )
         return render(request, 'charge.html')
+
+
+def _cart_id(request):
+	cart = request.session.session_key
+	if not cart:
+		cart = request.session.create()
+	return cart
+
+
+def add_cart(request, product_id):
+	product = Product.objects.get(id=product_id)
+	try:
+		cart = Cart.objects.get(cart_id=_cart_id(request))
+	except Cart.DoesNotExist:
+		cart = Cart.objects.create(
+				cart_id = _cart_id(request)
+			)
+		cart.save()
+	try:
+		cart_item = CartItem.objects.get(product=product, cart=cart)
+		if cart_item.quantity < cart_item.product.stock:
+			cart_item.quantity += 1
+			cart_item.save()
+		else:
+			messages.add_message(request, messages.INFO, 
+					'Sorry, no more of this item available')
+	except CartItem.DoesNotExist:
+		cart_item = CartItem.objects.create(
+					product = product,
+					quantity = 1,
+					cart = cart
+			)
+		cart_item.save()
+	return redirect('cart_detail')
+
+
+def cart_detail(request, total=0, counter=0, cart_items = None):
+	try:
+		cart = Cart.objects.get(cart_id=_cart_id(request))
+		cart_items = CartItem.objects.filter(cart=cart)
+		for cart_item in cart_items:
+			total += (cart_item.product.price * cart_item.quantity)
+			counter += cart_item.quantity
+	except ObjectDoesNotExist:
+		pass
+	return render(request, 'cart.html', dict(cart_items = cart_items, total = total, counter = counter))
+
+def cart_remove(request, product_id):
+	cart = Cart.objects.get(cart_id=_cart_id(request))
+	product = get_object_or_404(Product, id=product_id)
+	cart_item = CartItem.objects.get(product=product, cart=cart)
+	if cart_item.quantity > 1:
+		cart_item.quantity -= 1
+		cart_item.save()
+	else:
+		cart_item.delete()
+	return redirect('cart_detail')
+
+def full_remove(request):
+	cart = Cart.objects.get(cart_id=_cart_id(request))
+	cart_items = CartItem.objects.filter(cart=cart)
+	for cart_item in cart_items:
+		cart_item.delete()
+	return redirect('cart_detail')
