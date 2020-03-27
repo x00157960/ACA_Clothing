98e34ecd323f0427bd7bd6f3397549fa334e8501
x00157960 <x00157960@mytudublin.ie>
2020-03-27 22:04:45 +0000

    Fixed Categories AB#39

--- Code/shop/urls.py
+++ Code/shop/urls.py
@@ -6,7 +6,7 @@ from shop.views import productdetail
 urlpatterns = [
     path('', views.product_list,
             name='products'),
-    #path('<int:category_id>/', views.product_list,
-            #name='product_list_by_category'),
-    path('<int:id>/', productdetail, name='productdetail'),
+    path('<int:category_id>/', views.product_list,
+            name='product_list_by_category'),
+    path('product/<int:id>/', productdetail, name='productdetail'),
 ]
