fbc0ae4c8d91411654ce1bed590f594a54b01ada
x00157960 <x00157960@mytudublin.ie>
2020-04-03 12:40:45 +0100

    Attempted to Fix Reviews

--- Code/shop/models.py
+++ Code/shop/models.py
@@ -38,3 +38,28 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_one_star(self):
+        star_total = self.one_star + self.two_star + self.three_star + self.four_star + self.five_star
+        one_star_total = 100 * (self.one_star / star_total)
+        return one_star_total
+
+    def get_two_star(self):
+        star_total = self.one_star + self.two_star + self.three_star + self.four_star + self.five_star
+        two_star_total = 100 * (self.two_star / star_total)
+        return two_star_total
+
+    def get_three_star(self):
+        star_total = self.one_star + self.two_star + self.three_star + self.four_star + self.five_star
+        three_star_total = 100 * (self.three_star / star_total)
+        return three_star_total
+
+    def get_four_star(self):
+        star_total = self.one_star + self.two_star + self.three_star + self.four_star + self.five_star
+        four_star_total = 100 * (self.four_star / star_total)
+        return four_star_total
+
+    def get_five_star(self):
+        star_total = self.one_star + self.two_star + self.three_star + self.four_star + self.five_star
+        five_star_total = 100 * (self.five_star / star_total)
+        return five_star_total
