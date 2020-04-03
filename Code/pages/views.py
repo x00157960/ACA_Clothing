823d26adf8602b9f6061d8876d150ca0ad052ebb
Andy Finegan <andyfinegan@gmail.com>
2020-04-03 17:36:51 +0100

    added premium subscription + add to cart for product detail AB41

--- Code/pages/views.py
+++ Code/pages/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-
-
+from django.shortcuts import redirect
 from django.views.generic import TemplateView
+from shop.views import *
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
