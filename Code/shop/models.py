8aa603729404ac76803c881f0454a365789ee86d
Aaron James Kessie <x00156292@mytudublin.ie>
2020-04-27 14:41:43 +0100

    product variant models

--- Code/shop/models.py
+++ Code/shop/models.py
@@ -40,6 +40,32 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
+
+CHOOSE_SIZE = (
+('Extra Small', 'XS'),
+('Small', 'S'),
+('Medium', 'M'),
+('Large', 'L'),
+('Extra Large', 'XL'),
+('Double Large', 'XXL'),
+('Triple Large', 'XXXL'),
+)
+
+class ProductSize(models.Model):
+    product = models.ForeignKey(Product, "on_delete")
+    size = models.CharField(max_length=120, choices=CHOOSE_SIZE)
+    price = models.DecimalField(decimal_places=2, max_digits=20)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+            return self.size
+
+
+    def get_absolute_url(self):
+            return self.product.get_absolute_url()
+
+    
+
     def get_one_star(self):
         star_total = self.one_star + self.two_star + self.three_star + self.four_star + self.five_star
         one_star_total = 100 * (self.one_star / star_total)
